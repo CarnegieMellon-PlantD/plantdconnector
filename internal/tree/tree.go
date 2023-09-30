4d12ce8850d9696fca8056f989061ba4d1892593
Paul Shu <shuziteng1998@gmail.com>
2023-09-30 14:16:10 +0800

    Add helper functions

--- internal/tree/tree.go
+++ internal/tree/tree.go
@@ -1,13 +1,15 @@
 package tree
 
 import (
+	"fmt"
+
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 )
 
 type Node struct {
 	Name           string
-	Id             string
+	Id             pcommon.SpanID
 	StartTimestamp pcommon.Timestamp
 	EndTimestamp   pcommon.Timestamp
 	Children       []*Node
@@ -20,13 +22,13 @@ type Forest struct {
 func NewNode(span ptrace.Span) *Node {
 	return &Node{
 		Name:           span.Name(),
-		Id:             span.SpanID().String(),
+		Id:             span.SpanID(),
 		StartTimestamp: span.StartTimestamp(),
 		EndTimestamp:   span.EndTimestamp(),
 	}
 }
 
-func NewNodeWithId(id string) *Node {
+func NewNodeWithId(id pcommon.SpanID) *Node {
 	return &Node{
 		Id: id,
 	}
@@ -45,11 +47,11 @@ func NewForest() *Forest {
 }
 
 func (f *Forest) AddSpans(spans ptrace.SpanSlice) {
-	lookup := make(map[string]*Node)
+	lookup := make(map[pcommon.SpanID]*Node)
 
 	for i := 0; i < spans.Len(); i++ {
 		span := spans.At(i)
-		spanId := span.SpanID().String()
+		spanId := span.SpanID()
 		currNode, currExist := lookup[spanId]
 		if currExist {
 			currNode.Update(span)
@@ -58,7 +60,7 @@ func (f *Forest) AddSpans(spans ptrace.SpanSlice) {
 			lookup[spanId] = currNode
 		}
 
-		parentId := span.ParentSpanID().String()
+		parentId := span.ParentSpanID()
 		if span.ParentSpanID().IsEmpty() {
 			f.Roots = append(f.Roots, currNode)
 		} else {
@@ -69,5 +71,34 @@ func (f *Forest) AddSpans(spans ptrace.SpanSlice) {
 			}
 			parentNode.Children = append(parentNode.Children, currNode)
 		}
+	}
+}
+
+func NewNodeWithName(name string) *Node {
+	return &Node{
+		Name: name,
+	}
+}
+
+func (f *Forest) Print() {
+	newLineNode := NewNodeWithName("\n")
+	for _, n := range f.Roots {
+		q := make([]*Node, 2)
+		q[0] = n
+		q[1] = newLineNode
+		for len(q) > 0 {
+			top := q[0]
+			q = q[1:]
+			for _, child := range top.Children {
+				q = append(q, child)
+			}
+			if top.Name == "\n" && len(q) > 0 {
+				q = append(q, newLineNode)
+				fmt.Print("\n")
+			} else {
+				fmt.Printf("%s\t", top.Name)
+			}
+		}
+		fmt.Print("\n---------------\n")
 	}
 }
